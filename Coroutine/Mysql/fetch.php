6b2de1080f251aa82208f8833304848c79b30af8
沈唁 <52o@qq52o.cn>
2018-12-13 20:33:43 +0800

    Update fetch.php

--- Coroutine/Mysql/fetch.php
+++ Coroutine/Mysql/fetch.php
@@ -9,8 +9,8 @@ go(function(){
     		'user' => 'root',
     		'password' => 'root',
     		'database' => 'swoole_qq52o_me',
-		    // Ver >= 4.0-rc1 , 需在connect时加入 fetch_mode => true 选项
-		    'fetch_mode' => true
+		// Ver >= 4.0-rc1 , 需在connect时加入 fetch_mode => true 选项
+		'fetch_mode' => true
 	]);
 	$stmt = $db->prepare('select * from user_login limit 1');
 	$stmt->execute();
