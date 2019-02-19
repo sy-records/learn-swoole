8c63f5161ea2b7778aa4411ca677bcac220822d4
沈唁 <52o@qq52o.cn>
2019-02-19 21:22:53 +0800

    Update note.php

--- Swoole/note.php
+++ Swoole/note.php
@@ -4,5 +4,5 @@
 echo getmypid();
 
 // 设置进程名称
-swoole_set_process_name兼容性比cli_set_process_title要差，如果存在cli_set_process_title函数则优先使用cli_set_process_title。
+// swoole_set_process_name兼容性比cli_set_process_title要差，如果存在cli_set_process_title函数则优先使用cli_set_process_title。
 cli_set_process_title('swoole_server');
