ce75031598a616173a0a061a30a6ba5ddd1d0f70
沈唁 <52o@qq52o.cn>
2019-02-23 12:08:05 +0800

    Update note.php

--- Swoole/note.php
+++ Swoole/note.php
@@ -6,3 +6,8 @@ echo getmypid();
 // 设置进程名称
 // swoole_set_process_name兼容性比cli_set_process_title要差，如果存在cli_set_process_title函数则优先使用cli_set_process_title。
 cli_set_process_title('swoole_server');
+
+// php kill进程
+$pid = ’‘；
+// 其他信号见http://php.net/manual/zh/pcntl.constants.php
+posix_kill($pid, SIGKILL);
